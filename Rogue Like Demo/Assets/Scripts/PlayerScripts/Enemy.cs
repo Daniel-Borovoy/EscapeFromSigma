fca7be298d76ba7672194d4a2a85d5c67126492a
BadJuJa <74925631+BadJuJa@users.noreply.github.com>
2020-11-23 21:27:27 +0300

    Переносил скрипты

---
+++ Rogue Like Demo/Assets/Scripts/PlayerScripts/Enemy.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int health;
+    public float regenRate;
+    public float regenDelay;
+    private float timeToRegen = 5;
+
+
+    IEnumerator ExecuteAfterTime(float timeInSec)
+    {
+        yield return new WaitForSeconds(timeInSec);
+        Debug.Log("regenerating...");
+    }
+
+    private void Start()
+    {
+        if (health == 2)
+        {
+            Debug.Log(health);
+        }
+    }
+    private void Update()
+    {
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+
+        
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            StartCoroutine(ExecuteAfterTime(timeToRegen));
+        }
+    }
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+    }
+}
---
+++ Rogue Like Demo/Assets/Scripts/PlayerScripts/Gun.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform shotPoint;
+    
+    private float TimeBtwShots;
+    public float StartTimeBtwShots;
+
+
+    void Update()
+    {
+        Vector3 Difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float rotZ = Mathf.Atan2(Difference.y, Difference.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rotZ - 90);
+
+        if (TimeBtwShots <= 0)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                Instantiate(bullet, shotPoint.position, transform.rotation);
+                TimeBtwShots = StartTimeBtwShots;
+            }
+        }
+        else
+        {
+            TimeBtwShots -= Time.deltaTime;
+        }
+    }
+}
