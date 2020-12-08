bc80f749a2ca6a40346da7c96637ba1543457095
Daniel-Borovoy <daniel.borovoy.61@mail.ru>
2020-12-09 01:05:08 +0300

    готовый прототип меню

--- EscapeFromSigma/Assets/Main/Scripts/[Interface]/Menu.cs
+++ EscapeFromSigma/Assets/Main/Scripts/[Interface]/Menu.cs
@@ -6,7 +6,6 @@ public class Menu : MonoBehaviour
 {
     public static bool GameIsPaused = false;
     public GameObject pauseMenuUI;
-
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
@@ -20,19 +19,19 @@ public class Menu : MonoBehaviour
         		Pause();
         	}
         }
-
-        void Resume ()
-        {
-        	pauseMenuUI.SetActive(false);
-        	Time.timeScale = 1f;
-        	GameIsPaused = false;
-        }
-
-       	void Pause ()
-        {
-        	pauseMenuUI.SetActive(true);
-        	Time.timeScale = 0f;
-        	GameIsPaused = true;
-        }
     }
+    public void Resume ()
+        {
+            pauseMenuUI.SetActive(false);
+            Time.timeScale = 1f;
+            GameIsPaused = false;
+
+        }
+
+    public void Pause ()
+        {
+            pauseMenuUI.SetActive(true);
+            Time.timeScale = 0f;
+            GameIsPaused = true;
+        }
 }
---
+++ EscapeFromSigma/Assets/Main/Scripts/[Interface]/Settings.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Settings : MonoBehaviour
+{
+    bool isFullScreen = true;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void FullScreenToggle()
+    {
+        isFullScreen = !isFullScreen;
+        Screen.fullScreen = isFullScreen;
+    }
+    public void Quality(int q)
+    {
+        QualitySettings.SetQualityLevel(q);
+    }
+}
