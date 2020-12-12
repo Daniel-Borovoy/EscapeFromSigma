0b31cde4d90a592d97cb9857c3e7fb890a8ff937
Daniel-Borovoy <daniel.borovoy.61@mail.ru>
2020-12-12 20:46:56 +0300

    Промежуточный результат
    Добавил спавн врагов(пока не через плиту) и в процессе анимаця кнопок

---
+++ EscapeFromSigma/Assets/Main/Scripts/RandomSpawn.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSpawn : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject obj;
+    float RandX;
+    Vector2 whereToSpawn;
+    [SerializeField]
+    private float spawnRate = 1f;
+    float nextSpawn = 0.0f;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        if(Time.time > nextSpawn)
+        {
+            nextSpawn = Time.time + spawnRate;
+            RandX = Random.Range(-13.91f, 12.55f);
+            whereToSpawn = new Vector2(RandX, transform.position.y);
+            Instantiate(obj, whereToSpawn, Quaternion.identity);
+        }
+
+    }
+}
